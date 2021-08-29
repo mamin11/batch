10e9ddf11cc4640fe7c0aa26f892a31839ec875e
Amin <abdimamin@gmail.com>
2021-08-29 09:49:23 +0100

    added test batch job, switched to mysql db

--- src/main/java/com/batch/BatchApplication.java
+++ src/main/java/com/batch/BatchApplication.java
@@ -1,10 +1,46 @@
 package com.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchApplication {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step packageItemStep() {
+		return stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Item has been packaged - at step process");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+
+	@Bean
+	public Job deliverPackageJob() {
+		return jobBuilderFactory.get("deliverPackage").start(packageItemStep()).build();
+	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchApplication.class, args);
